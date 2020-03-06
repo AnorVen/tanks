d9d0989aa2c351e0a1b51f4c5ca939c24c826dec
Efremov Georgiy <g.efremov@rambler-co.ru>
2020-03-06 15:22:00 +0300

    start

---
+++ webpack.config.js
@@ -0,0 +1,63 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin'); //installed via npm
+const webpack = require('webpack'); //to access built-in plugins
+const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+
+module.exports = {
+  entry: './src/posts.js',
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.js'
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env", "@babel/preset-react"],
+            plugins: ["@babel/plugin-transform-runtime",
+              "babel-plugin-styled-components",
+              "@babel/plugin-proposal-class-properties",
+            ],
+          }
+        },
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-inline-loader?classPrefix'
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ],
+  },
+  devtool: 'inline-source-map',
+
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({template: './src/index.html'}),
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: false,
+    port: 9000,
+    historyApiFallback: true,
+  }
+}
