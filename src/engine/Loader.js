a52b981eed4147ad05329ff8e934b6ba0a332772
Efremov Georgiy <g.efremov@rambler-co.ru>
2020-03-06 19:53:46 +0300

    2-00

--- src/engine/Loader.js
+++ src/engine/Loader.js
@@ -1,31 +1,58 @@
-import { compilation } from "webpack";
-
 export class Loader {
   constructor() {
     this.loadOrder = {
       images: [],
-      json: []
+      jsons: []
     };
     this.resources = {
       images: [],
-      json: []
+      jsons: []
     };
   }
 
   addImage = (name, src) => {
     this.loadOrder.images.push({ name, src });
   };
+  addJson = (name, address) => {
+    this.loadOrder.jsons.push({ name, address });
+  };
 
-  load = (callback = () => ({})) => {
+  load = callback => {
+    this.loadImages(callback);
+    this.loadJsons(callback);
+  };
+
+  loadImages = callback => {
     const promises = [];
-    for (const { name, src } of this.loadOrder.images) {
-      const promise = Loader.loadImage(src).then(image => {
-        this.resources.images[name] = image;
-        if (this.loadOrder.images.includes(name)) {
-          const index = this.loadOrder.images.indexOf(name);
-          this.loadOrder.images.splice(index, 1);
-        }
-      });
+    for (const imageData of this.loadOrder.images) {
+      const { name, src } = imageData;
+      const promise = Loader.loadImage(src)
+        .then(image => {
+          this.resources.images[name] = image;
+          if (this.loadOrder.images.includes(imageData)) {
+            const index = this.loadOrder.images.indexOf(imageData);
+            this.loadOrder.images.splice(index, 1);
+          }
+        })
+        .catch(err => console.log(err));
+      promises.push(promise);
+    }
+    Promise.all(promises).then(() => callback());
+  };
+
+  loadJsons = callback => {
+    const promises = [];
+    for (const jsonData of this.loadOrder.jsons) {
+      const { name, address } = jsonData;
+      const promise = Loader.loadJson(address)
+        .then(res => {
+          this.resources.jsons[name] = res;
+          if (this.loadOrder.jsons.includes(jsonData)) {
+            const index = this.loadOrder.jsons.indexOf(jsonData);
+            this.loadOrder.jsons.splice(index, 1);
+          }
+        })
+        .catch(err => console.log(err));
       promises.push(promise);
     }
     Promise.all(promises).then(() => callback());
@@ -41,6 +68,24 @@ export class Loader {
         rej(err);
       }
     });
+  };
+  static loadJson = url => {
+    console.log(typeof url);
+    if (typeof url === "object") {
+      return new Promise((res, rej) => {
+        return res(url);
+      });
+    }
+    return fetch(url)
+      .then(res => {
+        console.log(res);
+        if (res.status === 200) {
+          console.log(res.json());
+          return res.json();
+        }
+        console.log(res.status);
+      })
+      .catch(error => console.log(error));
   };
 }
 
--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 require("./engine/Loader");
 import { Loader } from "./engine/Loader";
 import heroes from "./static/heroes.jpg";
+import persons from "./static/person.json";
 
 const loader = new Loader();
 loader.addImage("heroes", heroes);
-//loader.load(()=>console.log(1));
+loader.addJson("persons", persons);
+console.log(loader);
+const callback = () => {
+  console.log(loader);
+};
+//loader.load(callback);
 GameEngine.Loader.loadImage(heroes).then(image => {
   document.body.append(image);
 });
